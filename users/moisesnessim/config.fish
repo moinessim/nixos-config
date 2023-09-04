351be50a6f32577d37595d0a6e5de2e4e7f715be
Moises Nessim <moises.nessim@topmanage.com>
2023-09-03 17:56:17 -0700

    Avoid printing funcsave message

--- users/moisesnessim/config.fish
+++ users/moisesnessim/config.fish
@@ -77,7 +77,7 @@ end
 # Do not show any greeting
 set --universal --erase fish_greeting
 function fish_greeting; end
-funcsave fish_greeting
+funcsave fish_greeting > /dev/null
 
 # bobthefish theme
 set -g theme_color_scheme dracula
