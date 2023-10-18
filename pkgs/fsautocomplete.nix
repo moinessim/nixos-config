cfd7047475c8acb5c658d26cf2f6a605d017d596
Moises Nessim <moises.nessim@topmanage.com>
2023-10-18 12:38:27 -0500

    Support net6 and net7 in fsautocomplete

--- pkgs/fsautocomplete.nix
+++ pkgs/fsautocomplete.nix
@@ -1,10 +1,15 @@
-{ buildDotnetGlobalTool, lib }:
+{ buildDotnetGlobalTool, lib, dotnetCorePackages }:
 
 buildDotnetGlobalTool {
   pname = "fsautocomplete";
   version = "0.62.0";
 
   nugetSha256 = "sha256-p5A8WURcGbI8kgEVpvTYQnbcEGoIExPYA3FAR+bPM90=";
+
+  dotnet-runtime = with dotnetCorePackages; combinePackages [
+      sdk_7_0
+      sdk_6_0
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/fsharp/FsAutoComplete";
