47fc5109ca8bbc0ce3ada8ab1700163504abda4d
Moises Nessim <moises.nessim@topmanage.com>
2025-06-12 14:47:07 -0500

    qutebrowser: Act as Firefox for google accounts

--- users/moisesnessim/home-manager.nix
+++ users/moisesnessim/home-manager.nix
@@ -504,12 +504,18 @@ in {
         config.set("zoom.default", "100%")
         config.set("editor.command", ["kitty", "nvim", "{file}", "-c", "normal {line}G{column0}l"])
 
+        # https://github.com/qutebrowser/qutebrowser/issues/7427
+        config.set("content.headers.user_agent",
+                   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+                   "accounts.google.com")
+
         config.bind('<z><l>', 'spawn --userscript qute-pass ${args}')
         config.bind('<z><u><l>', 'spawn --userscript qute-pass ${args} --username-only')
         config.bind('<z><p><l>', 'spawn --userscript qute-pass ${args} --password-only')
         config.bind('<z><o><l>', 'spawn --userscript qute-pass ${args} --otp-only')
 
         config.bind('ca', 'set -t content.javascript.clipboard access ;; cmd-later 10s set -p content.javascript.clipboard none')
+
       '';
   };
 
