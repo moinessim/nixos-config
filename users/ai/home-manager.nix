a2d61b15d27acf121cf17026f9dfeb2c19f5621f
Moises Nessim <moises.nessim@topmanage.com>
2026-04-20 21:34:00 -0500

    ai: configure git prepare-commit-msg

--- users/ai/home-manager.nix
+++ users/ai/home-manager.nix
@@ -15,4 +15,18 @@
     OPENCODE_CONFIG_DIR = "/home/ai/.local/share/opencode-config";
     UMASK = "0002";
   };
+
+  programs.git = {
+    enable = true;
+    userName = "Moises Nessim";
+    userEmail = "moises.nessim@topmanage.com";
+    aliases = {
+      cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop\\|main' | xargs -n 1 -r git branch -d";
+      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      root = "rev-parse --show-toplevel";
+    };
+    hooks = {
+        prepare-commit-msg = ../moisesnessim/prepare-commit-msg;
+    };
+  };
 }
